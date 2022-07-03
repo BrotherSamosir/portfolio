fed9f5c3160d225d5e8f00a7598cd09d30531407
geraldsamosir <gerald.unforgiven@gmail.com>
2022-07-03 12:14:14 +0700

    feat(config): add rules

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rules: {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
-  }
 }
 
 module.exports = nextConfig
